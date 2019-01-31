ad37223db11178c6532246f91ec6e49b0762d1cd
MrBlue <irvynrega7@gmail.com>
2019-01-31 18:46:32 +0700

    Create a response Intent in the second Activity Add TextView elements to display the reply Get the reply from the Intent extra and display it

--- app/src/main/java/com/example/twoactivities/SecondActivity.java
+++ app/src/main/java/com/example/twoactivities/SecondActivity.java
@@ -4,11 +4,13 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
-
-    @Override
+public static final String EXTRA_REPLY = "com.example.twoactivities.extra.REPLY";
+private EditText mReply;
+@Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
@@ -19,5 +21,10 @@ public class SecondActivity extends AppCompatActivity {
     }
 
     public void returnReply(View view) {
+        String reply = mReply.getText().toString();
+        Intent replyintent = new Intent();
+        replyintent.putExtra(EXTRA_REPLY,reply);
+        setResult(RESULT_OK, replyintent);
+        finish();
     }
 }
